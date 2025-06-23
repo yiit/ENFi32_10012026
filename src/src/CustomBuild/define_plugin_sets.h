1cf3b1cf1c88efee13e774773408651aec20e3b3
chromoxdor <33860956+chromoxdor@users.noreply.github.com>
2025-06-23 16:17:50 +0200

    Update define_plugin_sets.h

--- src/src/CustomBuild/define_plugin_sets.h
+++ src/src/CustomBuild/define_plugin_sets.h
@@ -42,6 +42,9 @@ To create/register a plugin, you have to :
     #ifndef WEBSERVER_I2C_SCANNER
         #define WEBSERVER_I2C_SCANNER
     #endif
+    #ifndef WEBSERVER_CSVVAL
+      #define WEBSERVER_CSVVAL
+    #endif
     #ifndef WEBSERVER_FAVICON
         #define WEBSERVER_FAVICON
     #endif
@@ -511,7 +514,7 @@ To create/register a plugin, you have to :
         #define USES_P001   // switch
     #endif
     #ifndef USES_P026
-      #define USES_P026   // SysInfo
+//      #define USES_P026   // SysInfo
     #endif
     #ifndef USES_P033
       #define USES_P033   // Dummy
@@ -552,6 +555,12 @@ To create/register a plugin, you have to :
         #endif
         #ifdef WEBSERVER_I2C_SCANNER
             #undef WEBSERVER_I2C_SCANNER
+        #endif
+        #ifdef WEBSERVER_CSVVAL
+            #undef WEBSERVER_CSVVAL
+        #endif
+        #ifdef WEBSERVER_METRICS
+          #undef WEBSERVER_METRICS
         #endif
         #ifdef WEBSERVER_FAVICON
             #undef WEBSERVER_FAVICON
@@ -3525,7 +3534,11 @@ To create/register a plugin, you have to :
 #endif
 
 #ifndef FEATURE_TARSTREAM_SUPPORT
-  #define FEATURE_TARSTREAM_SUPPORT   1
+  #ifdef LIMIT_BUILD_SIZE
+    #define FEATURE_TARSTREAM_SUPPORT   0
+  #else
+    #define FEATURE_TARSTREAM_SUPPORT   1
+  #endif
 #endif // FEATURE_TARSTREAM_SUPPORT
 
 // Check for plugins that will use Extended Custom Settings storage when available
@@ -3614,7 +3627,18 @@ To create/register a plugin, you have to :
   #endif
 #endif
 
-
+#ifndef FEATURE_STRING_VARIABLES
+  #ifdef ESP32
+    #define FEATURE_STRING_VARIABLES  1
+  #endif
+  #ifdef ESP8266
+    #define FEATURE_STRING_VARIABLES  0
+  #endif
+#endif // ifndef FEATURE_STRING_VARIABLES
+#if FEATURE_STRING_VARIABLES && defined(ESP8266) // NOT supported on ESP8266 because of limited available memory
+  #undef FEATURE_STRING_VARIABLES
+  #define FEATURE_STRING_VARIABLES  0
+#endif
   
   
 #if !defined(CUSTOM_BUILD_CDN_URL) && !defined(FEATURE_ALTERNATIVE_CDN_URL)
@@ -3692,7 +3716,25 @@ To create/register a plugin, you have to :
   #define FEATURE_I2C_MULTIPLE    0 // NOT SUPPORTED
 #endif
 
-//-------------------HTTPResponseParser Section----------------
+#ifndef FEATURE_TASKVALUE_UNIT_OF_MEASURE
+  #ifdef ESP32
+    #define FEATURE_TASKVALUE_UNIT_OF_MEASURE   1
+  #endif
+  #ifdef ESP8266
+    #define FEATURE_TASKVALUE_UNIT_OF_MEASURE   0 // Disabled by default on ESP8266
+  #endif
+#endif
+
+#ifndef FEATURE_TASKVALUE_ATTRIBUTES
+  #ifdef ESP32
+    #define FEATURE_TASKVALUE_ATTRIBUTES  1
+  #endif
+  #ifdef ESP8266
+    #define FEATURE_TASKVALUE_ATTRIBUTES  0 // Disabled by default on ESP8266
+  #endif
+#endif // if FEATURE_TASKVALUE_ATTRIBUTES
+
+  //-------------------HTTPResponseParser Section----------------
 #ifndef FEATURE_THINGSPEAK_EVENT
   #if defined(PLUGIN_BUILD_MAX_ESP32)
     #define FEATURE_THINGSPEAK_EVENT 1
@@ -3724,17 +3766,17 @@ To create/register a plugin, you have to :
 #endif
 //-------------------End of HTTPResponseParser Section----------
 
-#if !(defined(SOC_DAC_SUPPORTED) && SOC_DAC_SUPPORTED)
-  #ifdef USES_P152
-    #undef USES_P152
+  #if !(defined(SOC_DAC_SUPPORTED) && SOC_DAC_SUPPORTED)
+    #ifdef USES_P152
+      #undef USES_P152
+    #endif
   #endif
-#endif
 
-#if !(defined(SOC_TOUCH_SENSOR_SUPPORTED) && SOC_TOUCH_SENSOR_SUPPORTED)
-  #ifdef USES_P097
-    #undef USES_P097
+  #if !(defined(SOC_TOUCH_SENSOR_SUPPORTED) && SOC_TOUCH_SENSOR_SUPPORTED)
+    #ifdef USES_P097
+      #undef USES_P097
+    #endif
   #endif
-#endif
 
 
 #endif // CUSTOMBUILD_DEFINE_PLUGIN_SETS_H
