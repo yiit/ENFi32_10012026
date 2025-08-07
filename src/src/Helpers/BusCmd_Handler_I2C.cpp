8e98e33d31f65a9bab1d69d5116f2aad6b3b80c6
Ton Huisman <tonhu2004@hotmail.com>
2025-08-07 21:17:27 +0200

    [BusCmd I2C] Add support for handling > 128 bytes on I2C read/write (ESP32)

--- src/src/Helpers/BusCmd_Handler_I2C.cpp
+++ src/src/Helpers/BusCmd_Handler_I2C.cpp
@@ -53,6 +53,17 @@ uint32_t BusCmd_Handler_I2C::read32uREG(uint16_t reg,
 std::vector<uint8_t>BusCmd_Handler_I2C::read8uB(uint32_t size) {
   std::vector<uint8_t> data_b;
 
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size));
+      # endif // ifndef BUILD_NO_DEBUG
+      return data_b;
+    }
+  }
+  #endif // ifdef ESP32
   data_b.reserve(size);
 
   _wire->requestFrom(_i2cAddress, size);
@@ -60,12 +71,29 @@ std::vector<uint8_t>BusCmd_Handler_I2C::read8uB(uint32_t size) {
   for (uint8_t i = 0; i < size; ++i) {
     data_b.push_back(_wire->read());
   }
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return data_b;
 }
 
 std::vector<uint16_t>BusCmd_Handler_I2C::read16uW(uint32_t size) {
   std::vector<uint16_t> data_w;
 
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size * 2)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size * 2));
+      # endif // ifndef BUILD_NO_DEBUG
+      return data_w;
+    }
+  }
+  #endif // ifdef ESP32
   data_w.reserve(size);
 
   _wire->requestFrom(_i2cAddress, size * 2);
@@ -73,6 +101,12 @@ std::vector<uint16_t>BusCmd_Handler_I2C::read16uW(uint32_t size) {
   for (uint8_t i = 0; i < size; ++i) {
     data_w.push_back((_wire->read() << 8) | _wire->read());
   }
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return data_w;
 }
 
@@ -81,6 +115,17 @@ std::vector<uint8_t>BusCmd_Handler_I2C::read8uBREG(uint16_t reg,
                                                    bool     wideReg) {
   std::vector<uint8_t> data_b;
 
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size));
+      # endif // ifndef BUILD_NO_DEBUG
+      return data_b;
+    }
+  }
+  #endif // ifdef ESP32
   data_b.reserve(size);
 
   if (I2C_write8(_i2cAddress, reg) && (_wire->requestFrom(_i2cAddress, size) == size)) {
@@ -88,6 +133,12 @@ std::vector<uint8_t>BusCmd_Handler_I2C::read8uBREG(uint16_t reg,
       data_b.push_back(_wire->read());
     }
   }
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return data_b;
 }
 
@@ -96,6 +147,17 @@ std::vector<uint16_t>BusCmd_Handler_I2C::read16uWREG(uint16_t reg,
                                                      bool     wideReg) {
   std::vector<uint16_t> data_w;
 
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size * 2)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size * 2));
+      # endif // ifndef BUILD_NO_DEBUG
+      return data_w;
+    }
+  }
+  #endif // ifdef ESP32
   data_w.reserve(size);
 
   if (I2C_write8(_i2cAddress, reg) && (_wire->requestFrom(_i2cAddress, size * 2) == size * 2)) {
@@ -103,26 +165,88 @@ std::vector<uint16_t>BusCmd_Handler_I2C::read16uWREG(uint16_t reg,
       data_w.push_back((_wire->read() << 8) | _wire->read());
     }
   }
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return data_w;
 }
 
 #if FEATURE_BUSCMD_STRING
-String BusCmd_Handler_I2C::readString(uint32_t len) { // FIXME Handle variable length
+String BusCmd_Handler_I2C::readString(uint32_t len) {
   String result;
 
-  result.reserve(len);
-  _wire->requestFrom(_i2cAddress, len);
+  # ifdef ESP32
 
-  for (uint8_t i = 0; i < len; ++i) {
-    result += static_cast<char>(_wire->read());
+  if (len > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(len)) { // 0 return on error
+      #  ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), len));
+      #  endif // ifndef BUILD_NO_DEBUG
+      return result;
+    }
   }
+  # endif // ifdef ESP32
+  result.reserve(len);
+
+  # ifdef ESP32
+  const size_t maxBytesPerChunk = len;
+  # endif // ifdef ESP32
+  # ifdef ESP8266
+  constexpr size_t maxBytesPerChunk = 128; // Chuncked in 32 byte blocks
+  # endif // ifdef ESP8266
+  size_t index = 0;
+
+  while (index < len) {
+    const size_t bytesRemaining = len - index;
+    const size_t bytesThisChunk = min(bytesRemaining, maxBytesPerChunk);
+    const size_t endIndex       = index + bytesThisChunk;
+    const bool   isLastChunk    = (bytesRemaining <= maxBytesPerChunk);
+    _wire->requestFrom(_i2cAddress, bytesThisChunk, isLastChunk);
+
+    for (; index < endIndex; ++index) {
+      # ifndef BUILD_NO_DEBUG
+
+      if (_wire->available() < 1) {
+        addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error reading data, got just: %u from %u requested bytes"), index, bytesThisChunk));
+        break;
+      }
+      # endif // ifndef BUILD_NO_DEBUG
+      const uint8_t c = _wire->read();
+
+      if (c != 0) {
+        result += static_cast<char>(c);
+      }
+    }
+  }
+
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  # endif // ifdef ESP32
   return result;
 }
 
 String BusCmd_Handler_I2C::readStringREG(uint16_t reg,
                                          uint32_t len,
-                                         bool     wideReg) { // FIXME Handle variable length
+                                         bool     wideReg) {
   String result;
+
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(len)) { // 0 return on error
+      #  ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), len));
+      #  endif // ifndef BUILD_NO_DEBUG
+      return result;
+    }
+  }
+  # endif // ifdef ESP32
 
   result.reserve(len);
   _wire->requestFrom(_i2cAddress, len);
@@ -132,6 +256,12 @@ String BusCmd_Handler_I2C::readStringREG(uint16_t reg,
       result += static_cast<char>(_wire->read());
     }
   }
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  # endif // ifdef ESP32
   return result;
 }
 
@@ -178,68 +308,182 @@ bool BusCmd_Handler_I2C::write32uREG(uint16_t reg,
 }
 
 uint32_t BusCmd_Handler_I2C::write8uB(std::vector<uint8_t>data) {
+  const size_t size = data.size();
+
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size));
+      # endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  #endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
 
-  for (size_t itb = 0; itb < data.size(); ++itb) {
+  for (size_t itb = 0; itb < size; ++itb) {
     _wire->write(data[itb]);
   }
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
 uint32_t BusCmd_Handler_I2C::write16uW(std::vector<uint16_t>data) {
+  const size_t size = data.size();
+
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size * 2)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size * 2));
+      # endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  #endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
 
-  for (size_t itw = 0; itw < data.size(); ++itw) {
+  for (size_t itw = 0; itw < size; ++itw) {
     _wire->write((uint8_t)(data[itw] << 8));
     _wire->write((uint8_t)(data[itw] & 0xFF));
   }
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
 uint32_t BusCmd_Handler_I2C::write8uBREG(uint16_t            reg,
                                          std::vector<uint8_t>data,
                                          bool                wideReg) {
+  const size_t size = data.size();
+
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size));
+      # endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  #endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
   _wire->write((uint8_t)reg);
 
-  for (size_t itb = 0; itb < data.size(); ++itb) {
+  for (size_t itb = 0; itb < size; ++itb) {
     _wire->write(data[itb]);
   }
+  #ifdef ESP32
+
+  if (size > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
 uint32_t BusCmd_Handler_I2C::write16uWREG(uint16_t             reg,
                                           std::vector<uint16_t>data,
                                           bool                 wideReg) {
+  const size_t size = data.size();
+
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(size * 2)) { // 0 return on error
+      # ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), size * 2));
+      # endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  #endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
   _wire->write((uint8_t)reg);
 
-  for (size_t itw = 0; itw < data.size(); ++itw) {
+  for (size_t itw = 0; itw < size; ++itw) {
     _wire->write((uint8_t)(data[itw] << 8));
     _wire->write((uint8_t)(data[itw] & 0xFF));
   }
+  #ifdef ESP32
+
+  if ((size * 2) > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  #endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
 #if FEATURE_BUSCMD_STRING
 uint32_t BusCmd_Handler_I2C::writeString(const String& data) {
+  const size_t len = data.length();
+
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(len)) { // 0 return on error
+      #  ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), len));
+      #  endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  # endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
 
-  for (size_t its = 0; its < data.length(); ++its) {
+  for (size_t its = 0; its < len; ++its) {
     _wire->write(data[its]);
   }
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  # endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
 uint32_t BusCmd_Handler_I2C::writeStringReg(uint16_t      reg,
                                             const String& data,
                                             bool          wideReg) {
+  const size_t len = data.length();
+
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    if (!_wire->setBufferSize(len)) { // 0 return on error
+      #  ifndef BUILD_NO_DEBUG
+      addLog(LOG_LEVEL_ERROR, strformat(F("I2C  : Error resizing buffer to size: %u"), len));
+      #  endif // ifndef BUILD_NO_DEBUG
+      return 0;
+    }
+  }
+  # endif // ifdef ESP32
   _wire->beginTransmission(_i2cAddress);
   _wire->write((uint8_t)reg);
 
-  for (size_t its = 0; its < data.length(); ++its) {
+  for (size_t its = 0; its < len; ++its) {
     _wire->write(data[its]);
   }
+  # ifdef ESP32
+
+  if (len > I2C_BUFFER_LENGTH) {
+    _wire->setBufferSize(I2C_BUFFER_LENGTH); // Restore default
+  }
+  # endif // ifdef ESP32
   return _wire->endTransmission() == 0;
 }
 
