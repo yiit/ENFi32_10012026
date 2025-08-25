d253f0a1795ab3084d01764031620c7accc28170
Ton Huisman <tonhu2004@hotmail.com>
2025-08-25 12:40:23 +0200

    [MQTT] Move Settings attribute to next available bit

--- src/src/DataStructs/SettingsStruct.h
+++ src/src/DataStructs/SettingsStruct.h
@@ -592,10 +592,10 @@ public:
     uint32_t ShowUnitOfMeasureOnDevicesPage   : 1; // Bit 07  // inverted
     uint32_t unused_08                        : 1; // Bit 08
     uint32_t unused_09                        : 1; // Bit 09
-    uint32_t MQTTConnectInBackground          : 1; // Bit 10  // inverted
+    uint32_t unused_10                        : 1; // Bit 10
     uint32_t unused_11                        : 1; // Bit 11
     uint32_t unused_12                        : 1; // Bit 12
-    uint32_t unused_13                        : 1; // Bit 13
+    uint32_t MQTTConnectInBackground          : 1; // Bit 13  // inverted
     uint32_t unused_14                        : 1; // Bit 14
     uint32_t unused_15                        : 1; // Bit 15
     uint32_t unused_16                        : 1; // Bit 16
